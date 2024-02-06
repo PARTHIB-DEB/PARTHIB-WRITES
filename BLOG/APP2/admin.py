f62fcd35fbca1a98a2d7e532b0c66dbbbe51e23f
PARTHIB KUMAR DEB <103876861+PARTHIB-DEB@users.noreply.github.com>
2024-02-06 21:08:08 +0530

    rm : Whole Old Project Architechture

--- BLOG/APP2/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from APP2.models import Blog
-
-# Register your models here.
-admin.site.register(Blog)
-
--- BLOG/APP2/forms.py
+++
@@ -1,7 +0,0 @@
-from django.forms import ModelForm
-from .models import *
-
-class Blogform(ModelForm):
-    class Meta:
-        model = Blog
-        fields = ['title', 'catch','image','details']
