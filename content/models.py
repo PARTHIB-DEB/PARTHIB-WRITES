b2b419215754e123180bc31eacbc47fecc0efd69
PARTHIB KUMAR DEB <103876861+PARTHIB-DEB@users.noreply.github.com>
2024-02-17 01:27:12 +0530

    Add : Usermodel , ContentModel , UserForm (Not Migrated)

--- content/models.py
+++ content/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class article(models.Model):
+    title = models.TextField()
+    script = models.CharField()
+    likes=models.IntegerField()
+    comments = models.IntegerField()
+    per_comment = models.CharField()
+    date = models.DateField(auto_now=False, auto_now_add=False)
+    
+    def __str__(self) -> str:
+        return self.title
--- users/models.py
+++ users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from content.models import *
 
 # Create your models here.
+
+class engagements(models.Model):
+    username = models.ForeignKey(User,null=True,blank=True, related_name="uname",on_delete=models.CASCADE)
+    likes = models.ForeignKey(article, null=True,blank=True, related_name="like", on_delete=models.CASCADE)
+    comments = models.ForeignKey(article, null=True,blank=True, related_name="comment", on_delete=models.CASCADE)
+    
+    def __str__(self) -> str:
+        return self.username
