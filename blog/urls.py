2969c44e688c098adfd0bfda87dceedc55480648
PARTHIB KUMAR DEB <103876861+PARTHIB-DEB@users.noreply.github.com>
2024-02-22 14:33:49 +0530

    Add : Urls for Blogs

--- blog/urls.py
+++ blog/urls.py
@@ -20,6 +20,6 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('users.urls')),
-    path('blog/',include('content.urls')),
+    path('blogs/',include('content.urls')),
     path("__reload__/", include("django_browser_reload.urls"))
 ]
