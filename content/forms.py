72bd1a0faa4b899efc6448bbd6b9845978dc6879
PARTHIB KUMAR DEB <103876861+PARTHIB-DEB@users.noreply.github.com>
2024-02-23 21:06:43 +0530

    Add : Form for like-comment  , deleteion

--- content/forms.py
+++ content/forms.py
@@ -1,7 +1,15 @@
-from django.forms import ModelForm
-from .models import articleCreateModel
+from django.forms import ModelForm , forms
+from .models import *
 
 class articleForm(ModelForm):
     class Meta:
         model = articleCreateModel
         fields = "__all__"
+
+class DeleteArticleForm(forms):
+    question = forms.forms.CharField(max_length=True)
+    
+class LikeCommentForm(ModelForm):
+    class Meta:
+        model = articleViewModel
+        fields = ("per_like","per_comment")
