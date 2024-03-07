a98cffd9f1278a0dc12dc7a55d61024b95c26cd9
PARTHIB KUMAR DEB <103876861+PARTHIB-DEB@users.noreply.github.com>
2024-03-07 22:01:10 +0530

    upd : Media files Urls Updated

--- blog/settings.py
+++ blog/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -136,6 +137,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'thumbs/')
+MEDIA_URL = '/thumbs/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
@@ -146,3 +150,7 @@ COMPRESS_ROOT = BASE_DIR / 'static' # new
 COMPRESS_ENABLED = True # new
 
 STATICFILES_FINDERS = ('compressor.finders.CompressorFinder',) # new
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
