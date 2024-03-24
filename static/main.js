e430db6dd75df5ff9fc193c8a49074243b90c68e
PARTHIB KUMAR DEB <103876861+PARTHIB-DEB@users.noreply.github.com>
2024-03-24 14:34:37 +0530

    upd : changes in  Templates

--- static/main.js
+++ static/main.js
@@ -2,6 +2,7 @@
 let elem = document.getElementById('ham')
 let ham = document.getElementById('navItems')
 let main = document.getElementsByTagName('main')
+let nav_blog = document.getElementById('nav-blog')
 
 const rotate=()=> {
     if (elem.style.transform === 'rotate(0deg)') {
@@ -17,3 +18,11 @@ const rotate=()=> {
     }
 }
 elem.addEventListener("click",rotate);
+
+
+nav_blog.addEventListener("click",function () {
+  document.querySelector("body").style.transitionTimingFunction = 'ease'
+  document.querySelector("body").style.transitionDuration = '2s'
+//   document.querySelector("body").style.scrollBehavior = "smooth"
+  document.querySelector("body").scroll(0,100)
+})
