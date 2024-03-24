44fefc7fcbb05883b60b85c83d7e557a4f8c4376
PARTHIB KUMAR DEB <103876861+PARTHIB-DEB@users.noreply.github.com>
2024-03-24 14:34:12 +0530

    Upd: Test database Engine

--- blog/settings.py
+++ blog/settings.py
@@ -91,6 +91,7 @@ DATABASES = {
         'HOST': '127.0.0.1',
         'PORT': '5432',
         'TEST': {
+            'ENGINE': 'django.db.backends.postgresql',
             'NAME': 'test_personal_blog',
         }
     }
