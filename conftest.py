362c4bc215a60605c887b36848c61ffc6fe5e0a2
PARTHIB KUMAR DEB <103876861+PARTHIB-DEB@users.noreply.github.com>
2024-03-26 13:15:14 +0530

    upd : Usermode Testkit logic

--- conftest.py
+++ conftest.py
@@ -1,61 +1,81 @@
 import pytest
-
 from django.urls import reverse
+from pytest_django.fixtures import django_user_model
 from users.models import *
-from users.forms import *
+from users.forms import UserForm, LoginForm
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth import authenticate
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
-Sender_Email = os.getenv('EMAIL_HOST_USER')
-Sender_Email_Password = os.getenv('EMAIL_HOST_PASSWORD')
+Sender_Email = os.getenv("EMAIL_HOST_USER")
+Sender_Email_Password = os.getenv("EMAIL_HOST_PASSWORD")
+usermodel = django_user_model(db="test_personal_blog")
 
-
-# Testing will be done for these functions
-# Signin (register)  , Signout (Delete) , Login , Logout , Likes and Comments (Probably)
-# Probably a Class will be best for this case
 
 @pytest.mark.django_db
 class TestUserModel:
-  def __init__(self , **data) -> None:
-    form = UserCreationForm(data)
-    user_data = form.cleaned_data
-    self.username = user_data.get("username"),
-    self.email = user_data.get("email"),
-    self.password = user_data.get("password"),
-    self.first_name = user_data.get("first_name"),
-    self.last_name = user_data.get("last_name")
-    
-  @pytest.fixture()
-  def create_a_user(self,django_user_model):
-      if self.email == Sender_Email:
-            user_obj = django_user_model.objects.create_superuser(
-                username = self.username,
-                email = self.email,
-                password = self.password,
-                first_name = self.first_name,
-                last_name = self.last_name,
+
+    @pytest.fixture(scope="class")
+    def __init__(self, **data) -> None:
+        form = UserForm(data=data)
+        if form.is_valid():
+            user_data = form.cleaned_data
+            self.username = (user_data.get("username"),)
+            self.email = (user_data.get("email"),)
+            self.password = (user_data.get("password"),)
+            self.first_name = (user_data.get("first_name"),)
+            self.last_name = (user_data.get("last_name"),)
+            self.is_logged_in = 0
+
+    @pytest.fixture(scope="class")
+    def create_a_user(self):
+        if self.email == Sender_Email:
+            user_obj = usermodel.objects.create_superuser(
+                username=self.username,
+                email=self.email,
+                password=self.password,
+                first_name=self.first_name,
+                last_name=self.last_name,
             )
             assert user_obj.is_superuser == True
-            return user_obj['username']
-      else:
-            user_obj = django_user_model.objects.create_superuser(
-                username = self.username,
-                email = self.email,
-                password = self.password,
-                first_name = self.first_name,
-                last_name = self.last_name,
+            return user_obj.username
+        else:
+            user_obj = usermodel.objects.create_user(
+                username=self.username,
+                email=self.email,
+                password=self.password,
+                first_name=self.first_name,
+                last_name=self.last_name,
             )
             assert user_obj.is_superuser == True
-            return user_obj['username']
-        
-  def update_a_user(self,django_user_model,create_a_user : str,**details):
-      user_obj = django_user_model.objects.filter(username = create_a_user).update(**details)
-      return user_obj
-  
-  def update_a_user_password(self,django_user_model, data , create_a_user : str):
-      pass
-  
-    
-      
+            yield user_obj.username
+            usermodel.objects.filter(username=user_obj.username).all().delete()
+
+    def update_a_user(self, data, create_a_user: str):
+        prev_user_obj = usermodel.objects.filter(username=create_a_user).all()
+        form = UserForm(data=data, instance=prev_user_obj)
+        if form.is_valid():
+            new_data = form.cleaned_data
+            assert new_data not in prev_user_obj == True
+            form.save()
+
+    def update_a_user_password(self, data, create_a_user: str):
+        prev_user_obj = usermodel.objects.filter(username=create_a_user).all()
+        form = PasswordChangeForm(data=data, instance=prev_user_obj)
+        if form.is_valid():
+            new_password = form.clean_new_password2()
+            assert new_password != prev_user_obj.password
+            form.save()
+
+    def login_a_user(self, data):
+        form = LoginForm(data=data)
+        user_data = form.data
+        user_obj = authenticate(
+            username=user_data["username"], password=user_data["password"]
+        )
+        if user_obj is not None:
+            self.is_logged_in = 1
+        assert self.is_logged_in == 1
