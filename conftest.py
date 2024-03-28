9226388ea3f21a3bd0ead9866e90ac7cb7fb879d
PARTHIB KUMAR DEB <103876861+PARTHIB-DEB@users.noreply.github.com>
2024-03-29 00:13:47 +0530

    Add : conftest for blogs and upd : conftest logic for blogs

--- conftest.py
+++ conftest.py
@@ -1,8 +1,9 @@
 import pytest
 from django.urls import reverse
-from pytest_django.fixtures import django_user_model
 from users.models import *
 from users.forms import UserForm, LoginForm
+from content.models import *
+from content.forms import *
 from django.contrib.auth.forms import PasswordChangeForm
 from django.contrib.auth import authenticate
 from dotenv import load_dotenv
@@ -12,66 +13,119 @@ load_dotenv()
 
 Sender_Email = os.getenv("EMAIL_HOST_USER")
 Sender_Email_Password = os.getenv("EMAIL_HOST_PASSWORD")
-usermodel = django_user_model
 
 
 
 class ConfUserModel:
     
-    @pytest.fixture
-    def create_a_user(self,**data):
+    django_user_model = newUser
+    
+    @pytest.fixture(scope='class')
+    def take_data(self,**data):
         form = UserForm(data=data)
         if form.is_valid():
-            user_data = form.cleaned_data
-            self.username = (user_data.get("username"),)
-            self.email = (user_data.get("email"),)
-            self.password = (user_data.get("password"),)
-            self.first_name = (user_data.get("first_name"),)
-            self.last_name = (user_data.get("last_name"),)
-            self.is_logged_in = 0
-            if self.email == Sender_Email:
-                user_obj = usermodel.objects.create_superuser(
-                    username=self.username,
-                    email=self.email,
-                    password=self.password,
-                    first_name=self.first_name,
-                    last_name=self.last_name,
+            return data
+        
+    @pytest.fixture(scope='function')
+    def create_a_user(self,**old_data):
+            if old_data.get("email") == Sender_Email:
+                user_obj = self.django_user_model.objects.create_superuser(
+                    username=old_data.get("username"),
+                    email=old_data.get("email"),
+                    password=old_data.get("password"),
+                    first_name=old_data.get("first_name"),
+                    last_name=old_data.get("last_name"),
                 )
                 assert user_obj.is_superuser == True
-                return user_obj.username
+                return throw_name(old_data.get("username"))
             else:
-                user_obj = usermodel.objects.create_user(
-                    username=self.username,
-                    email=self.email,
-                    password=self.password,
-                    first_name=self.first_name,
-                    last_name=self.last_name,
+                user_obj = self.django_user_model.objects.create_user(
+                    username=old_data.get("username"),
+                    email=old_data.get("email"),
+                    password=old_data.get("password"),
+                    first_name=old_data.get("first_name"),
+                    last_name=old_data.get("last_name"),
                 )
-                assert user_obj.is_superuser == True
-                return user_obj.username
+                assert user_obj.is_superuser == False
+                return old_data.get("username")
 
-    def update_a_user(self, data ,create_a_user:str):
-        prev_user_obj = usermodel.objects.filter(username=create_a_user).all()
-        form = UserForm(data=data, instance=prev_user_obj)
+    def update_a_user(self, data:dict , **old_data):
+        form = UserForm(data=data)
         if form.is_valid():
-            new_data = form.cleaned_data
-            assert new_data not in prev_user_obj == True
-            form.save()
+            clean_data = form.clean()
+            self.django_user_model.objects.filter(username = old_data.get("username")).update(
+                username = clean_data.get("username"),
+                email = clean_data.get("email"),
+                password = clean_data.get("password"),
+                first_name = clean_data.get("first_name"),
+                last_name = clean_data.get("last_name")
+            )
+            assert old_data != data
 
-    def update_a_user_password(self, data, create_a_user: str):
-        prev_user_obj = usermodel.objects.filter(username=create_a_user).all()
-        form = PasswordChangeForm(data=data, instance=prev_user_obj)
+    def update_a_user_password(self, data, **old_data):
+        user_obj = self.django_user_model.objects.filter(username=old_data.get('username')).all()
+        form = PasswordChangeForm(data=data, user=old_data.get('username'))
         if form.is_valid():
             new_password = form.clean_new_password2()
-            assert new_password != prev_user_obj.password
             form.save()
+            assert new_password != user_obj.password
 
-    def login_a_user(self, data):
+    def login_a_user(self, data , **old_data):
         form = LoginForm(data=data)
         user_data = form.data
         user_obj = authenticate(
             username=user_data["username"], password=user_data["password"]
         )
-        if user_obj is not None:
-            self.is_logged_in = 1
-        assert self.is_logged_in == 1
+        if user_obj:
+            form.save()
+            assert True
+
+
+class ConfBlogModel :
+    
+    blog = articleCreateModel
+    view_blog = articleViewModel
+    viewer = newUser
+    
+    @pytest.fixture(scope='class')
+    def take_data(self,**data):
+        form = UserForm(data=data)
+        if form.is_valid():
+            return data
+        
+    
+    def create_a_blog(self,**old_data):
+        blog = self.blog.objects.create(**old_data)
+        blog.save()
+        assert self.blog.objects.all().count() > 0
+    
+    def update_a_blog(self , data:dict , **old_data):
+        form = articleForm(data=data)
+        if form.is_valid():
+            clean_data = form.clean()
+            blog_obj = self.blog.objects.get(title = old_data.get("title"))
+            if clean_data.get("title") != blog_obj.title:
+                blog_obj.title = clean_data.get("title")
+            if clean_data.get("catchline") != blog_obj.catchline:
+                blog_obj.catchline = clean_data.get("catchline")
+            if clean_data.get("script") != blog_obj.script:
+                blog_obj.script = clean_data.get("script")
+            blog_obj.save()
+            assert old_data != data
+    
+    # def retrieve_a_blog(self ,**old_data):
+    #     try:
+    #         uname = self.viewer.objects.get(email = Sender_Email).username
+    #         x = self.view_blog.objects.update_or_create(
+    #             btitle_id = self.blog.objects.get(title=old_data.get("title")),
+    #             btitle = self.blog.objects.get(title=old_data.get("title")).title,
+    #             user_id = self.viewer.objects.filter(username = uname).first(),
+    #             username = uname
+    #         )
+    #         x.save()
+    #         assert self.view_blog.objects.all().count() > 0
+            
+    #     except Exception as error:
+    #         print(error)
+        
+        
